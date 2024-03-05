a1fd8a53b89c1c5bc3c6f441a4261af842a28e5f
Fabian Silva <silvabravofabian@gmail.com>
2024-03-05 16:46:20 -0400

    seccion de contacto iniciada

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,13 +1,54 @@
 import Image from "next/image";
 import { NavBarComp } from "../components/NavBar";
 import { WsSticky } from "../components/WsSticky";
+import { Card } from "flowbite-react";
 
 export default function Contacto() {
   return (
     <>
       <NavBarComp></NavBarComp>
       <main className="justify-center grid bg-gradient-to-r from-orange-200 to-red-400 p-2">
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4"></div>
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/img/qr1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </Card>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/img/qr1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </Card>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/img/qr1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
         <WsSticky></WsSticky>
       </main>
     </>
