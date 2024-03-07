8ca38dd38b2dc0fb1dbdbdfcfd4db4c0de940518
Fabian Silva <silvabravofabian@gmail.com>
2024-03-06 22:47:52 -0400

    agregado links

--- app/components/WsSticky.jsx
+++ app/components/WsSticky.jsx
@@ -1,11 +1,17 @@
 import { Footer } from "flowbite-react";
+import Link from "next/link";
 
 import { FaWhatsappSquare } from "react-icons/fa";
 
 export function WsSticky() {
   return (
-    <Footer className="justify-center fixed bottom-1 right-2 z-20 w-fit p-1 bg-white border-t border-gray-200 shadow ">
-      <FaWhatsappSquare className="text-5xl" color="green" />
-    </Footer>
+    <Link
+      target="_blank"
+      href={"https://wa.me/584126443690?text=Hola, vengo de su pagina web!"}
+    >
+      <Footer className="justify-center fixed bottom-1 right-2 z-20 w-fit p-1 bg-white border-t border-gray-200 shadow ">
+        <FaWhatsappSquare className="text-7xl" color="green" />
+      </Footer>
+    </Link>
   );
 }
--- app/contacto/page.js
+++ app/contacto/page.js
@@ -2,55 +2,50 @@ import Image from "next/image";
 import { NavBarComp } from "../components/NavBar";
 import { WsSticky } from "../components/WsSticky";
 import { Card } from "flowbite-react";
+import Link from "next/link";
 
 export default function Contacto() {
   return (
-    <>
+    <div className="h-ful">
       <NavBarComp></NavBarComp>
-      <main className="justify-center grid bg-gradient-to-r from-orange-200 to-red-400 p-2">
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
-          <Card
-            className="max-w-sm"
-            imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="/img/qr1.jpg"
+      <main className="justify-center grid bg-gradient-to-r h-full from-orange-200 to-red-400 p-2">
+        <div className="grid md:grid-cols-2 sm:grid-cols-2 gap-4">
+          <Link target="_bank" href={"https://www.instagram.com/salome17_ca"}>
+            <Card
+              className="max-w-sm"
+              imgAlt="Meaningful alt text for an image that is not purely decorative"
+              imgSrc="/img/qr1.jpg"
+            >
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Instagram
+              </h5>
+              <p className="font-normal text-xl text-gray-700 dark:text-gray-400">
+                @salome17_ca
+              </p>
+            </Card>
+          </Link>
+          <Link
+            target="_blank"
+            href={
+              "https://wa.me/584126443690?text=Hola, vengo de su pagina web!"
+            }
           >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </Card>
-          <Card
-            className="max-w-sm"
-            imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="/img/qr1.jpg"
-          >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </Card>
-          <Card
-            className="max-w-sm"
-            imgAlt="Meaningful alt text for an image that is not purely decorative"
-            imgSrc="/img/qr1.jpg"
-          >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </Card>
+            <Card
+              className="max-w-sm"
+              imgAlt="Meaningful alt text for an image that is not purely decorative"
+              imgSrc="/img/qr1.jpg"
+            >
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                WhatsApp
+              </h5>
+              <p className="font-normal text-xl text-gray-700 dark:text-gray-400">
+                0412-6443690
+              </p>
+            </Card>
+          </Link>
         </div>
         <WsSticky></WsSticky>
       </main>
-    </>
+    </div>
   );
 }
