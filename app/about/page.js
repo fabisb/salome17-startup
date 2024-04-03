f7d18f5f3dbb583df3faf2ccd2dc37a2a68c54d6
Fabian Silva <silvabravofabian@gmail.com>
2024-04-03 13:33:34 -0400

    terminadas alertas al agregar producto al carrito

--- app/about/page.js
+++ app/about/page.js
@@ -9,23 +9,34 @@ export default function About() {
   return (
     <>
       <NavBarComp></NavBarComp>
-      <main className="justify-center grid bg-gradient-to-r from-orange-200 to-red-400 p-2">
-        <Card
-          className="max-w-sm"
-          renderImage={() => (
-            <Image width={500} height={500} src="/img/icon.jpg" alt="image 1" />
-          )}
-        >
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Salomé 17
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Brindamos asesoria y soluciones en productos de quincalleria y
-            bolsas plasticas.
-          </p>
-        </Card>
-        <WsSticky></WsSticky>
+      <main className="   bg-gradient-to-r from-orange-200 to-red-400 p-2">
+        <div className="container flex justify-center">
+          <Card
+            className="max-w-md"
+            imgAlt="Salome 17 logo"
+            imgSrc="/img/icon.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Salomé 17
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Brindamos asesoria y soluciones en productos de quincalleria y
+              bolsas plasticas.
+            </p>
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Idea de emprendimiento.
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Este emprendimiento consiste en distribuir y cubrir las
+              necesidades en los siguientes productos de bolsas plásticas,
+              quincallería, útiles de limpieza, artículos de papelería y
+              productos desechables de manera más eficiente y con excelente
+              precio.
+            </p>
+          </Card>
+        </div>
       </main>
+      <WsSticky></WsSticky>
     </>
   );
 }
--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,35 +1,38 @@
 "use client";
-
 import { TextInput } from "flowbite-react";
 import Image from "next/image";
-
 import React, { useState } from "react";
+import { HiBadgeCheck } from "react-icons/hi";
 
 export default function ProductCard(props) {
   const [cantidad, setCantidad] = useState(0);
-
+  const [btnVali, setBtnVali] = useState(false);
   const addProduct = (nombre, precio, id) => {
     try {
-      
-      if (cantidad <= 0 ) {
-        return alert('Ingrese una cantidad valida')
+      if (cantidad <= 0) {
+        return alert("Ingrese una cantidad valida");
+      }
+      const producto = { nombre, precio, cantidad, id };
+      const productosGuardados =
+        JSON.parse(localStorage.getItem("productos")) || [];
+      const existente = productosGuardados.findIndex((prod) => prod.id === id);
+      if (existente != -1) {
+        productosGuardados[existente].cantidad = cantidad;
+      } else {
+        productosGuardados.push(producto);
+      }
+      // Guarda el arreglo actualizado en el localStorage
+      localStorage.setItem("productos", JSON.stringify(productosGuardados));
+      console.log("🚀 ~ addProduct ~ productosGuardados:", productosGuardados);
+      setBtnVali(true);
+      setTimeout(() => {
+        console.log("Timeout");
+        setBtnVali(false);
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+      return alert("Ha ocurrido un error");
     }
-    const producto = { nombre, precio, cantidad, id };
-    const productosGuardados =
-      JSON.parse(localStorage.getItem("productos")) || [];
-    const existente = productosGuardados.findIndex((prod) => prod.id === id);
-    if (existente != -1) {
-      productosGuardados[existente].cantidad = cantidad;
-    } else {
-      productosGuardados.push(producto);
-    }
-    // Guarda el arreglo actualizado en el localStorage
-    localStorage.setItem("productos", JSON.stringify(productosGuardados));
-    console.log("🚀 ~ addProduct ~ productosGuardados:", productosGuardados);
-  } catch (error) {
-    console.log(error);
-    return alert('Ha ocurrido un error');
-  }
   };
 
   return (
@@ -39,7 +42,7 @@ export default function ProductCard(props) {
           width={800}
           height={800}
           className="w-full h-auto p-8 rounded-t-lg"
-          src={"/productos/"+props.imagen}
+          src={"/productos/" + props.imagen}
           alt="product image"
         />
       </a>
@@ -60,12 +63,18 @@ export default function ProductCard(props) {
             min={0}
             onChange={(event) => setCantidad(event.target.value)}
           ></TextInput>
-          <a
-            onClick={() => addProduct(props.name, props.precio, props.id)}
-            className="hover:cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Agregar al carrito
-          </a>
+          {btnVali == false ? (
+            <a
+              onClick={() => addProduct(props.name, props.precio, props.id)}
+              className="hover:cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Agregar al carrito
+            </a>
+          ) : (
+            <a className="hover:cursor-pointer text-white bg-green-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+              Agregado! <HiBadgeCheck size={20} />
+            </a>
+          )}
         </div>
       </div>
     </div>
